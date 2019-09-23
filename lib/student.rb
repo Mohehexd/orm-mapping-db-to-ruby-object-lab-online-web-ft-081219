7bcb076e2ce92a95adc8a76ce2643082c6c524d2
Guillermo Martinez <dumblemoe@gmail.com>
2019-09-23 01:13:58 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -9,16 +9,14 @@ class Student
     new_student
   end
 
-  def self.all
-    
-    sql = <<-SQL
-      SELECT *
-      FROM songs
+def self.all
+ 
+    FROM students 
     SQL
     DB[:conn].execute(sql).collect do |row|
       self.new_from_db(row)
     end
-  end
+  end	  
 
   def self.find_by_name(name)
     sql = <<-SQL
