5cc715e5184681104e5fac94064cf9493f04577c
Guillermo Martinez <dumblemoe@gmail.com>
2019-09-23 01:04:57 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -15,10 +15,17 @@ class Student
   end
 
   def self.find_by_name(name)
+    sql = <<-SQL
+      SELECT *
+      FROM Student
+      WHERE name = ?
+      LIMIT 1
+    SQL
+ 
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
 
-    
-
-  end
+  end.first 
   
   def save
     sql = <<-SQL
