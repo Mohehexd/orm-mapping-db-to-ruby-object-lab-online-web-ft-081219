372cd85abe921d4e77b9baa5bae166e6de817bcb
Guillermo Martinez <dumblemoe@gmail.com>
2019-09-23 01:33:53 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -96,9 +96,10 @@ end
   def self.students_below_12th_grade
     
     sql = <<-SQL
-    SELECT *
-    FROM students 
-    WHERE grade < 12
-    
+      SELECT *
+      FROM students 
+      WHERE grade < 12
+    SQL
+    DB[:conn].execute(sql)
   end 
 end
